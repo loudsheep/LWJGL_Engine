bc1b93a1e17b64b484b613bf3d38dc5b53aedb72
loudsheep <rochmykietow@gmail.com>
2021-03-28 19:37:34 +0200

    Added Camera class

---
+++ src/main/java/engine/Camera.java
@@ -0,0 +1,37 @@
+package engine;
+
+import org.joml.Matrix4f;
+import org.joml.Vector2f;
+import org.joml.Vector3f;
+
+public class Camera {
+    private Matrix4f projectionMatrix, viewMatrix;
+    private Vector2f position;
+
+    public Camera(Vector2f position) {
+        this.position = position;
+        this.projectionMatrix = new Matrix4f();
+        this.viewMatrix = new Matrix4f();
+        adjustProjetion();
+    }
+
+    public void adjustProjetion() {
+        projectionMatrix.identity();
+        projectionMatrix.ortho(0, 32 * 40f, 0, 32 * 21f, 0, 100);
+    }
+
+    public Matrix4f getViewMatrix() {
+        Vector3f cameraFront = new Vector3f(0f, 0f, -1f);
+        Vector3f cameraUp = new Vector3f(0f, 1f, 0f);
+        this.viewMatrix.identity();
+        viewMatrix.lookAt(new Vector3f(position.x, position.y, 20f),
+                cameraFront.add(position.x, position.y, 0f),
+                cameraUp);
+
+        return this.viewMatrix;
+    }
+
+    public Matrix4f getProjectionMatrix() {
+        return projectionMatrix;
+    }
+}
