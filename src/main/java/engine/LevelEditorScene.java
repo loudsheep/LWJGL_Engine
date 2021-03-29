3d83f886f0be425792f2ea401a1ed8da98521237
loudsheep <rochmykietow@gmail.com>
2021-03-29 11:00:32 +0200

    Added methods to upload variables in Shader class

--- src/main/java/engine/LevelEditorScene.java
+++ src/main/java/engine/LevelEditorScene.java
@@ -3,6 +3,7 @@ package engine;
 import org.joml.Vector2f;
 import org.lwjgl.BufferUtils;
 import render.Shader;
+import util.Time;
 
 import java.nio.FloatBuffer;
 import java.nio.IntBuffer;
@@ -109,6 +110,7 @@ public class LevelEditorScene extends Scene {
         defaultShader.use();
         defaultShader.uploadMat4f("uProjection", camera.getProjectionMatrix());
         defaultShader.uploadMat4f("uView", camera.getViewMatrix());
+        defaultShader.uploadFloat("uTime", Time.getTime());
         // Bind the VAO that we're using
         glBindVertexArray(vaoID);
 
