9f0147c76bfc595a8382c233c4b1feba48ce0f9c
loudsheep <rochmykietow@gmail.com>
2021-03-29 14:37:02 +0200

    Created Texture class to display textures

--- assets/shaders/default.glsl
+++ assets/shaders/default.glsl
@@ -2,15 +2,18 @@
 #version 330 core
 layout (location=0) in vec3 aPos;
 layout (location=1) in vec4 aColor;
+layout (location=2) in vec2 aUV;
 
 uniform mat4 uProjection;
 uniform mat4 uView;
 
 out vec4 fColor;
+out vec2 fUV;
 
 void main()
 {
     fColor = aColor;
+    fUV = aUV;
     gl_Position = uProjection * uView * vec4(aPos, 1.0);
 }
 
@@ -18,13 +21,14 @@ void main()
 #version 330 core
 
 uniform float uTime;
+uniform sampler2D TEX_SAMPLER;
 
 in vec4 fColor;
+in vec2 fUV;
 
 out vec4 color;
 
 void main()
 {
-    float avg = (fColor.r + fColor.g + fColor.b) / 3;
-    color = vec4(avg, avg, avg, 1);
+    color = texture(TEX_SAMPLER, fUV);
 }
