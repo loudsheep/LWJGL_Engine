8c9db75cff81b10a21903f3a074c890485f0f0f5
loudsheep <rochmykietow@gmail.com>
2021-04-07 14:52:32 +0200

    Started exposing variable to LevelEditor

--- src/main/java/engine/LevelEditorScene.java
+++ src/main/java/engine/LevelEditorScene.java
@@ -2,6 +2,7 @@ package engine;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import components.Rigidbody;
 import components.Sprite;
 import components.SpriteRenderer;
 import components.Spritesheet;
@@ -27,7 +28,8 @@ public class LevelEditorScene extends Scene {
     public void init() {
         loadResources();
         this.camera = new Camera(new Vector2f(-250, 0));
-        if(levelLoaded) {
+        if (levelLoaded) {
+            this.activeGameObject = gameObjects.get(0);
             return;
         }
 
@@ -38,6 +40,7 @@ public class LevelEditorScene extends Scene {
         SpriteRenderer obj1Sprite = new SpriteRenderer();
         obj1Sprite.setColor(new Vector4f(1, 0, 0, 1));
         obj1.addComponent(obj1Sprite);
+        obj1.addComponent(new Rigidbody());
         this.addGameObjectToScene(obj1);
         this.activeGameObject = obj1;
 
@@ -58,6 +61,7 @@ public class LevelEditorScene extends Scene {
 
         AssetPool.addSpritesheet("assets/images/spritesheet.png", new Spritesheet(AssetPool.getTexture("assets/images" +
                 "/spritesheet.png"), 16, 16, 26, 0));
+        AssetPool.getTexture("assets/images/blendImage2.png");
     }
 
     @Override
