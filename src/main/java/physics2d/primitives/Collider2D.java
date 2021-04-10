9dc21d0b394f049056d5afb122bf2a7432637f10
loudsheep <rochmykietow@gmail.com>
2021-04-10 19:59:03 +0200

    PHYSICS - Created basic primitives object

---
+++ src/main/java/physics2d/primitives/Collider2D.java
@@ -0,0 +1,10 @@
+package physics2d.primitives;
+
+import components.Component;
+import org.joml.Vector2f;
+
+public class Collider2D extends Component {
+    protected Vector2f offSet = new Vector2f();
+
+//    public abstract float getInertiaTensor(float mass);
+}
