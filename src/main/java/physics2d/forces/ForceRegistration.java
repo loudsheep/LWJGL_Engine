cc4394d0aba73893bc798f8e17f524c9911b5a1e
loudsheep <rochmykietow@gmail.com>
2021-04-18 17:40:55 +0200

    PHYSICS - Force generators

---
+++ src/main/java/physics2d/forces/ForceRegistration.java
@@ -0,0 +1,22 @@
+package physics2d.forces;
+
+import physics2d.rigidbody.Rigidbody2D;
+
+public class ForceRegistration {
+    public ForceGenerator fg;
+    public Rigidbody2D rb;
+
+    public ForceRegistration(ForceGenerator fg, Rigidbody2D rb) {
+        this.fg = fg;
+        this.rb = rb;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) return false;
+        if (!(obj instanceof ForceRegistration)) return false;
+
+        ForceRegistration fr = (ForceRegistration) obj;
+        return fr.rb == this.rb && fr.fg == this.rb;
+    }
+}
