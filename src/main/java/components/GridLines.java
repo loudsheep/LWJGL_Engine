21d747f26f6489dd6254d16f239d3618755342eb
loudsheep <rochmykietow@gmail.com>
2021-04-26 08:15:34 +0200

    Moving and zooming cameras in level editor scene

--- src/main/java/components/GridLines.java
+++ src/main/java/components/GridLines.java
@@ -1,5 +1,6 @@
 package components;
 
+import engine.Camera;
 import engine.Window;
 import org.joml.Vector2f;
 import org.joml.Vector3f;
@@ -9,17 +10,18 @@ import util.Settings;
 public class GridLines extends Component {
     @Override
     public void update(float dt) {
-        Vector2f caperaPosition = Window.getScene().camera().position;
-        Vector2f projectionSize = Window.getScene().camera().getProjectionSize();
+        Camera camera = Window.getScene().camera();
+        Vector2f caperaPosition = camera.position;
+        Vector2f projectionSize = camera.getProjectionSize();
 
         int firstX = (int) ((caperaPosition.x / Settings.GRID_WIDTH - 1)) * Settings.GRID_WIDTH;
         int firstY = (int) ((caperaPosition.y / Settings.GRID_HEIGHT - 1)) * Settings.GRID_HEIGHT;
 
-        int numVerLines = (int) (projectionSize.x / Settings.GRID_WIDTH) + 2;
-        int numHorLines = (int) (projectionSize.y / Settings.GRID_HEIGHT) + 2;
+        int numVerLines = (int) (projectionSize.x * camera.getZoom() / Settings.GRID_WIDTH) + 2;
+        int numHorLines = (int) (projectionSize.y * camera.getZoom() / Settings.GRID_HEIGHT) + 2;
 
-        int width = (int) projectionSize.x + Settings.GRID_WIDTH * 2;
-        int height = (int) projectionSize.y + Settings.GRID_HEIGHT * 2;
+        int width = (int) (projectionSize.x * camera.getZoom()) + Settings.GRID_WIDTH * 2;
+        int height = (int) (projectionSize.y * camera.getZoom()) + Settings.GRID_HEIGHT * 2;
 
         int maxLines = Math.max(numVerLines, numHorLines);
         Vector3f color = new Vector3f(0.2f, 0.2f, 0.2f);
--- src/main/java/engine/Camera.java
+++ src/main/java/engine/Camera.java
@@ -7,7 +7,9 @@ import org.joml.Vector3f;
 public class Camera {
     private Matrix4f projectionMatrix, viewMatrix, inverseProjection, inverseView;
     public Vector2f position;
-    private Vector2f projectionSize = new Vector2f(32 * 40f, 32 * 21f);
+    private Vector2f projectionSize = new Vector2f(32.0f * 40.0f, 32.0f * 21.0f);
+
+    private float zoom = 1.0f;
 
     public Camera(Vector2f position) {
         this.position = position;
@@ -15,21 +17,22 @@ public class Camera {
         this.viewMatrix = new Matrix4f();
         this.inverseProjection = new Matrix4f();
         this.inverseView = new Matrix4f();
-        adjustProjetion();
+        adjustProjection();
     }
 
-    public void adjustProjetion() {
+    public void adjustProjection() {
         projectionMatrix.identity();
-        projectionMatrix.ortho(0, projectionSize.x, 0, projectionSize.y, 0, 100);
+        projectionMatrix.ortho(0.0f, projectionSize.x * zoom,
+                0.0f, projectionSize.y * zoom, 0.0f, 100.0f);
         projectionMatrix.invert(inverseProjection);
     }
 
     public Matrix4f getViewMatrix() {
-        Vector3f cameraFront = new Vector3f(0f, 0f, -1f);
-        Vector3f cameraUp = new Vector3f(0f, 1f, 0f);
+        Vector3f cameraFront = new Vector3f(0.0f, 0.0f, -1.0f);
+        Vector3f cameraUp = new Vector3f(0.0f, 1.0f, 0.0f);
         this.viewMatrix.identity();
-        viewMatrix.lookAt(new Vector3f(position.x, position.y, 20f),
-                cameraFront.add(position.x, position.y, 0f),
+        viewMatrix.lookAt(new Vector3f(position.x, position.y, 20.0f),
+                cameraFront.add(position.x, position.y, 0.0f),
                 cameraUp);
         this.viewMatrix.invert(inverseView);
 
@@ -37,18 +40,30 @@ public class Camera {
     }
 
     public Matrix4f getProjectionMatrix() {
-        return projectionMatrix;
+        return this.projectionMatrix;
     }
 
     public Matrix4f getInverseProjection() {
-        return inverseProjection;
+        return this.inverseProjection;
     }
 
     public Matrix4f getInverseView() {
-        return inverseView;
+        return this.inverseView;
     }
 
     public Vector2f getProjectionSize() {
-        return projectionSize;
+        return this.projectionSize;
+    }
+
+    public float getZoom() {
+        return zoom;
+    }
+
+    public void setZoom(float zoom) {
+        this.zoom = zoom;
+    }
+
+    public void addZoom(float value) {
+        this.zoom += value;
     }
 }
