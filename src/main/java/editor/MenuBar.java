827f3e660eda5d5e1a6d77ef959dd92b5adb6d11
loudsheep <rochmykietow@gmail.com>
2021-05-04 09:22:14 +0200

    Created scene hierarcgy window

--- src/main/java/editor/MenuBar.java
+++ src/main/java/editor/MenuBar.java
@@ -7,20 +7,20 @@ import observers.events.EventType;
 
 public class MenuBar {
     public void imgui() {
-        ImGui.beginMainMenuBar();
+        ImGui.beginMenuBar();
 
         if (ImGui.beginMenu("File")) {
             if (ImGui.menuItem("Save", "Ctrl+S")) {
                 EventSystem.notify(null, new Event(EventType.SaveLevel));
             }
 
-            if(ImGui.menuItem("Load", "Ctrl+O")) {
+            if (ImGui.menuItem("Load", "Ctrl+O")) {
                 EventSystem.notify(null, new Event(EventType.LoadLevel));
             }
 
             ImGui.endMenu();
         }
 
-        ImGui.endMainMenuBar();
+        ImGui.endMenuBar();
     }
 }
